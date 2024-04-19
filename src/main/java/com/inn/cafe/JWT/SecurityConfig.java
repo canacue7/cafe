3c50a26d7e08522799a7464cd15330c2a5591c1a
carlos canacue <carlosduardcar@gmail.com>
2024-04-18 22:42:49 -0500

    configure token

---
+++ src/main/java/com/inn/cafe/JWT/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.inn.cafe.JWT;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
