ccf7e2838df15f0c37a44aabf1582e2dcc63a651
carlos canacue <carlosduardcar@gmail.com>
2024-04-23 21:35:05 -0500

    adding get categories

--- src/main/java/com/inn/cafe/restImpl/CategoryRestImpl.java
+++ src/main/java/com/inn/cafe/restImpl/CategoryRestImpl.java
@@ -1,5 +1,6 @@
 package com.inn.cafe.restImpl;
 
+import com.inn.cafe.POJO.Category;
 import com.inn.cafe.constents.CafeConstants;
 import com.inn.cafe.rest.CategoryRest;
 import com.inn.cafe.service.CategoryService;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,5 +28,15 @@ public class CategoryRestImpl implements CategoryRest {
             e.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<Category>> getAllCategories(String filterValue) {
+        try{
+            return categoryService.getAllCategory(filterValue);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
@@ -6,14 +6,19 @@ import com.inn.cafe.constents.CafeConstants;
 import com.inn.cafe.dao.CategoryDao;
 import com.inn.cafe.service.CategoryService;
 import com.inn.cafe.utils.CafeUtils;
+import com.google.common.base.Strings;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-
+//this is to printo some logs in our console
+@Slf4j
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
@@ -34,8 +39,8 @@ public class CategoryServiceImpl implements CategoryService {
             }else{
                 return CafeUtils.getResponseEntity(CafeConstants.UNAUTHORIZED_ACCESS, HttpStatus.UNAUTHORIZED);
             }
-        }catch (Exception e){
-            e.printStackTrace();
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -58,5 +63,19 @@ public class CategoryServiceImpl implements CategoryService {
         }
         category.setName(requestMap.get("name"));
         return category;
+    }
+
+    @Override
+    public ResponseEntity<List<Category>> getAllCategory(String filterValue) {
+        try{
+            if (!Strings.isNullOrEmpty(filterValue) && filterValue.equalsIgnoreCase("true")){
+                log.info("Inside If");
+                return new ResponseEntity<List<Category>>(categoryDao.getAllCategory(), HttpStatus.OK);
+            }
+            return new ResponseEntity<>(categoryDao.findAll(), HttpStatus.OK);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<List<Category>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
